56fc00d866e43d48b0873cdfc467c444120b1c4c
PRABHAT KUMAR <96784632+PrabhatPrajapati@users.noreply.github.com>
2023-11-02 19:19:13 +0530

    Update main.py

--- main.py
+++ main.py
@@ -28,6 +28,43 @@ import pickle
 import streamlit as st
 import requests
 
+col1, col2, col3, col4, col5 = st.columns(5)
+with col1:
+    st.markdown("Do Visit @: ")
+with col2:
+    st.markdown(
+        "[Linkedin](https://www.linkedin.com/in/prabhat-kumar-prajapati/)")
+with col3:
+    st.markdown("[Github](https://github.com/PrabhatPrajapati)")
+with col4:
+    st.markdown("[leetcode](https://leetcode.com/prabhat1999/)")
+
+st.sidebar.title("Movie Recommender System")
+
+st.sidebar.caption(
+    'This type of recommendation systems, takes in a movie that a user currently likes as input. Then it analyzes the contents (storyline, genre, cast, director etc.) of the movie to find out other movies which have similar content. Then it ranks similar movies according to their similarity scores and recommends the most relevant movies to the user.')
+
+st.sidebar.title("Dataset")
+st.sidebar.caption('The following main data source was used for this project:')
+
+st.sidebar.markdown("[Movie dataset](https://www.kaggle.com/datasets/tmdb/tmdb-movie-metadata?select=tmdb_5000_movies.csv)")
+
+st.sidebar.title("Data Pre-processing")
+st.sidebar.caption('''
+* Dropping columns that are not required
+* Merging dataframes''')
+
+st.sidebar.title("Cosine Similarity")
+st.sidebar.caption('''
+Also known as vector-based similarity, this formulation views two items and their ratings as vectors, and defines the similarity between them as the angle between these vectors:''')
+
+
+st.sidebar.title("Recommender")
+st.sidebar.caption('''
+* User enters his favourite movie (or the movie on the basis of which he wants the system to recommend movies)
+* Based on the user's input, recommendation is made by sorting the similarities in descending order''')
+
+
 def fetch_poster(movie_id):
     url = "https://api.themoviedb.org/3/movie/{}?api_key=8265bd1679663a7ea12ac168da84d2e8&language=en-US".format(movie_id)
     data = requests.get(url)
